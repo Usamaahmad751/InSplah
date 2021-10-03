8881c251349ea11471afa24e649a880e5be0dac5
Usama <Usamaahmad751@gmail.com>
2021-10-03 13:42:49 +0500

    first commit

---
+++ precache-manifest.53348a2b79c1e4dac9451e02d889e14a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb716ef464366acaa20e065780cd19d6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9180be8b2d23ae384c80",
+    "url": "./static/css/2.eacbdd6c.chunk.css"
+  },
+  {
+    "revision": "2cbb13d81a35351af805",
+    "url": "./static/css/main.0fd564a1.chunk.css"
+  },
+  {
+    "revision": "9180be8b2d23ae384c80",
+    "url": "./static/js/2.0353cc2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0353cc2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cbb13d81a35351af805",
+    "url": "./static/js/main.96b174ca.chunk.js"
+  },
+  {
+    "revision": "1835b8375ddbc48c0411",
+    "url": "./static/js/runtime-main.ad6d7bcd.js"
+  }
+]);
